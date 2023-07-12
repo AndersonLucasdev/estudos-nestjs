6356bc4c49fb95c8e2362cabda6792d1bc96a3ed
anderson <andinlucas8@gmail.com>
2023-07-12 10:14:12 -0300

    FEAT/ implementando o migrations com prisma

---
+++ prisma/migrations/20230712121946_add_tables_and_columns/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "creationDate" TIMESTAMP(3) NOT NULL,
+    "gender" TEXT NOT NULL,
+    "birthDate" TIMESTAMP(3) NOT NULL,
+    "profilePhoto" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
