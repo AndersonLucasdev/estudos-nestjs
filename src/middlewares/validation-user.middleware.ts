bd3df6c06f21dbcdba6ca0fb2e1a55d75ab37922
anderson <andinlucas8@gmail.com>
2023-07-13 19:49:52 -0300

    FEAT/ implementando middlewares de validação user

---
+++ src/middlewares/validation-user.middleware.ts
@@ -0,0 +1,15 @@
+import { Injectable, NestMiddleware, BadRequestException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class ValidationUserMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    // Realize a validação dos dados de entrada
+    const { username, name, email, password, phone, gender, birthDate, profilePhoto } = req.body;
+
+    if (!username || !name || !email || !password) {
+      throw new BadRequestException('Campos obrigatórios estão faltando');
+    }
+    next();
+  }
+}
--- src/user/modules/user.module.ts
+++ src/user/modules/user.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserController } from '../controllers/user.controller';
 import { UserService } from '../services/user.service';
+import { ValidationUserMiddleware } from 'src/middlewares/validation-user.middleware';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+      consumer.apply(ValidationUserMiddleware).forRoutes('users');
+    }
+  }
