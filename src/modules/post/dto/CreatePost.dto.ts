5ccef8861cadc7c039c72c76011559b9d959307f
anderson <andinlucas8@gmail.com>
2023-07-20 10:16:26 -0300

    FEAT/ construindo dto post

---
+++ src/modules/post/dto/CreatePost.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IsString,
+  IsEmail,
+  IsDate,
+  IsEnum,
+  MinLength,
+  Matches,
+  IsOptional,
+  isEmail,
+  IsInt,
+  IsBoolean
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsInt()
+  userId: number;
+
+  @IsString()
+  image?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsBoolean()
+  disableComments: boolean;
+
+  @IsInt()
+  likes: number;
+}
---
+++ src/modules/post/dto/PatchPost.dto.ts
@@ -0,0 +1,31 @@
+import {
+    IsString,
+    IsEmail,
+    IsDate,
+    IsEnum,
+    MinLength,
+    Matches,
+    IsOptional,
+    isEmail,
+    IsInt,
+    IsBoolean
+  } from 'class-validator';
+  
+  export class PatchPostDto {
+    @IsInt()
+    userId: number;
+  
+    @IsString()
+    @IsOptional()
+    image?: string;
+  
+    @IsString()
+    @IsOptional()
+    description?: string;
+  
+    @IsBoolean()
+    disableComments: boolean;
+  
+    @IsInt()
+    likes: number;
+  }
