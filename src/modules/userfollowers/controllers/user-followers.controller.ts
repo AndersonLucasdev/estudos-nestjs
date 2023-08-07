67342cdbec4fe3283ac755efbfa647f55e3268e7
anderson <andinlucas8@gmail.com>
2023-08-07 18:44:51 -0300

    FIX(userfollowers)/ corrigindo erros nas pastas

---
+++ src/modules/userfollowers/controllers/user-followers.controller.ts
@@ -0,0 +1,28 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Query,
+  Body,
+  Param,
+  ParseIntPipe,
+  UsePipes,
+  NotFoundException,
+  HttpStatus,
+  ConflictException,
+  BadRequestException,
+  HttpException,
+} from '@nestjs/common';
+import { UserFollowersService } from '../services/user-followers.service';
+
+import { DtoValidationPipe } from 'src/pipes/dto-validation.pipe';
+import { formatUserData } from 'src/utils/FormartUserData';
+import * as bcrypt from 'bcrypt';
+
+@Controller('comment-like')
+export class CommentLikeController {
+  constructor(private readonly userFollowersService: UserFollowersService) {}
+
+}
--- src/modules/userfollowers/services/user-followers.service.ts
+++ src/modules/userfollowers/services/user-followers.service.ts
@@ -6,10 +6,10 @@ import {
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserFollowers } from '@prisma/client';
+import { TrimSpaces } from 'src/utils/helpers';
 
 @Injectable()
 export class UserFollowersService {
-    constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  
 }
