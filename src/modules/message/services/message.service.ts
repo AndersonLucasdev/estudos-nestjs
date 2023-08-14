cd43a555248e4ec093a43a6a957a4a444986bcc3
anderson <andinlucas8@gmail.com>
2023-08-14 18:42:46 -0300

    FEAT(message)/ finalizando parcialmente o service

--- src/modules/message/services/message.service.ts
+++ src/modules/message/services/message.service.ts
@@ -8,103 +8,104 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { Message } from '@prisma/client';
 import { CreateMessageDto } from '../dto/CreateMessage.dto';
 import { TrimSpaces } from 'src/utils/helpers';
+import { Conversation } from '@prisma/client';
 
 @Injectable()
 export class MessageService {
   constructor(private readonly prisma: PrismaService) {}
 
-  // async sendMessage(createMessageDto: CreateMessageDto): Promise<Message> {
-  //   const { senderId, recipientId, content } = createMessageDto;
+  async sendMessage(createMessageDto: CreateMessageDto): Promise<Message> {
+    const { senderId, recipientId, content } = createMessageDto;
 
-  //   // Create the message in the database
-  //   const newMessage = await this.prisma.message.create({
-  //     data: {
-  //       sender: { connect: { id: senderId } },
-  //       recipient: { connect: { id: recipientId } },
-  //       content: TrimSpaces(content),
-  //     },
-  //   });
+    // Create the message in the database
+    const newMessage = await this.prisma.message.create({
+      data: {
+        sender: { connect: { id: senderId } },
+        recipient: { connect: { id: recipientId } },
+        content: TrimSpaces(content),
+      },
+    });
 
-  //   return newMessage;
-  // }
+    return newMessage;
+  }
 
-  // async deleteMessage(messageId: number): Promise<void> {
-  //   const message = await this.prisma.message.findUnique({
-  //     where: { id: messageId },
-  //   });
+  async deleteMessage(messageId: number): Promise<void> {
+    const message = await this.prisma.message.findUnique({
+      where: { id: messageId },
+    });
 
-  //   if (!message) {
-  //     throw new NotFoundException(`Message with ID ${messageId} not found`);
-  //   }
+    if (!message) {
+      throw new NotFoundException(`Message with ID ${messageId} not found`);
+    }
 
-  //   // Delete the message
-  //   await this.prisma.message.delete({
-  //     where: { id: messageId },
-  //   });
-  // }
+    // Delete the message
+    await this.prisma.message.delete({
+      where: { id: messageId },
+    });
+  }
 
-  // async updateMessage(messageId: number, content: string): Promise<Message> {
-  //   const existingMessage = await this.prisma.message.findUnique({
-  //     where: { id: messageId },
-  //   });
+  async updateMessage(messageId: number, content: string): Promise<Message> {
+    const existingMessage = await this.prisma.message.findUnique({
+      where: { id: messageId },
+    });
 
-  //   if (!existingMessage) {
-  //     throw new NotFoundException(`Message with ID ${messageId} not found`);
-  //   }
+    if (!existingMessage) {
+      throw new NotFoundException(`Message with ID ${messageId} not found`);
+    }
 
-  //   // Update the message's content
-  //   const updatedMessage = await this.prisma.message.update({
-  //     where: { id: messageId },
-  //     data: { content: TrimSpaces(content) },
-  //   });
+    // Update the message's content
+    const updatedMessage = await this.prisma.message.update({
+      where: { id: messageId },
+      data: { content: TrimSpaces(content) },
+    });
 
-  //   return updatedMessage;
-  // }
+    return updatedMessage;
+  }
 
-  // async getUserConversations(userId: number): Promise<Conversation[]> {
-  //   const userConversations = await this.prisma.user.findUnique({
-  //     where: { id: userId },
-  //     include: {
-  //       conversations: {
-  //         include: {
-  //           participants: true,
-  //           messages: true,
-  //         },
-  //       },
-  //     },
-  //   });
+  async getUserConversations(userId: number): Promise<Conversation[]> {
+    const userConversations = await this.prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        conversations: {
+          include: {
+            participants: true,
+            messages: true,
+          },
+        },
+      },
+    });
 
-  //   return userConversations.conversations;
-  // }
+    return userConversations.conversations;
+  }
 
-  // async getAllMessagesInConversation(conversationId: number): Promise<Message[]> {
-  //   const messages = await this.prisma.conversation.findUnique({
-  //     where: { id: conversationId },
-  //     include: {
-  //       messages: {
-  //         include: {
-  //           sender: true,
-  //           recipient: true,
-  //         },
-  //       },
-  //     },
-  //   });
+  async getAllMessagesInConversation(conversationId: number): Promise<Message[]> {
+    const messages = await this.prisma.conversation.findUnique({
+      where: { id: conversationId },
+      include: {
+        messages: {
+          include: {
+            sender: true,
+            recipient: true,
+          },
+        },
+      },
+    });
 
-  //   return messages.messages;
-  // }
+    return messages.messages;
+  }
 
-  // async getUserMessagesInConversation(userId: number, conversationId: number): Promise<Message[]> {
-  //   const userMessages = await this.prisma.message.findMany({
-  //     where: {
-  //       senderId: userId,
-  //       conversationId,
-  //     },
-  //     include: {
-  //       sender: true,
-  //       recipient: true,
-  //     },
-  //   });
+  async getUserMessagesInConversation(userId: number, conversationId: number): Promise<Message[]> {
+    const userMessages = await this.prisma.message.findMany({
+      where: {
+        senderId: userId,
+        conversationId,
+      },
+      include: {
+        sender: true,
+        recipient: true,
+      },
+    });
 
-  //   return userMessages;
-  // }
+    return userMessages;
+  }
 }
