c4a0dbb0a156aca3aa187a17a4f0b56e888e80d8
anderson <andinlucas8@gmail.com>
2024-03-20 19:12:59 -0300

    feat/ auth.controllers.ts: Adicionando swagger

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,19 +1,36 @@
-import { Controller, Post, HttpCode, HttpStatus, UnauthorizedException, UseGuards, Body } from "@nestjs/common"; // Adicione "Body" ao import
-import { AuthService } from "../services/auth.service";
-import { LocalAuthGuard } from "../guards/local-auth.guard";
+import {
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  UnauthorizedException,
+  UseGuards,
+  Body,
+} from '@nestjs/common'; // Adicione "Body" ao import
+import { AuthService } from '../services/auth.service';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
+import {
+  ApiTags,
+  ApiResponse,
+  ApiBadRequestResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
-@Controller('auth') 
+@Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post()
-    // @UseGuards(LocalAuthGuard)
-    @HttpCode(HttpStatus.OK)
-    async login(@Body() loginData: { username: string, password: string }) {
-        const user = await this.authService.validateUser(loginData.username, loginData.password);
-        if (!user) {
-            throw new UnauthorizedException('Usuário ou senha inválidos');
-        }
-        return this.authService.login(user);
+  @Post()
+  // @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() loginData: { username: string; password: string }) {
+    const user = await this.authService.validateUser(
+      loginData.username,
+      loginData.password,
+    );
+    if (!user) {
+      throw new UnauthorizedException('Usuário ou senha inválidos');
     }
+    return this.authService.login(user);
+  }
 }
