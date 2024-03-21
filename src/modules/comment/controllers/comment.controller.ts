b8e6b0be46a02c269c2b69f9552e9f472fcbcd11
anderson <andinlucas8@gmail.com>
2024-03-21 20:26:13 -0300

    feat/ comment.controllers.ts: Continuando a documentação swagger

--- src/modules/comment/controllers/comment.controller.ts
+++ src/modules/comment/controllers/comment.controller.ts
@@ -24,6 +24,8 @@ import {
   ApiResponse,
   ApiBadRequestResponse,
   ApiUnauthorizedResponse,
+  ApiNotFoundResponse,
+  ApiParam
 } from '@nestjs/swagger';
 
 @Controller('comments')
@@ -32,6 +34,9 @@ export class CommentController {
 
   // Endpoint to get a comment by its ID
   @Get(':id')
+  @ApiParam({ name: 'id', description: 'Comment ID' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Comment found', type: Comment })
+  @ApiNotFoundResponse({ description: 'Comment not found' })
   async getCommentById(@Param('id', ParseIntPipe) id: number) {
     try {
       const comment = await this.commentService.getCommentById(id);
@@ -46,6 +51,9 @@ export class CommentController {
 
   // Endpoint to get all comments for a specific post
   @Get('posts/:postId')
+  @ApiParam({ name: 'postId', description: 'Post ID' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Comments found', type: [Comment] })
+  @ApiNotFoundResponse({ description: 'Comments not found' })
   async getAllPostComments(@Param('id', ParseIntPipe) id: number) {
     try {
       const comments = await this.commentService.GetAllPostComments(id);
@@ -57,6 +65,9 @@ export class CommentController {
 
   // Endpoint to get all comments made by a specific user
   @Get('users/:userId')
+  @ApiParam({ name: 'userId', description: 'User ID' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Comments found', type: [Comment] })
+  @ApiNotFoundResponse({ description: 'Comments not found' })
   async getAllUserComments(@Param('id', ParseIntPipe) id: number) {
     try {
       const comments = await this.commentService.GetAllUserComments(id);
@@ -68,6 +79,8 @@ export class CommentController {
 
   // Endpoint to get the most recent comments up to a specified limit
   @Get('recent')
+  @ApiResponse({ status: HttpStatus.OK, description: 'Recent comments found', type: [Comment] })
+  @ApiNotFoundResponse({ description: 'Comments not found' })
   async getRecentComments(@Query('limit', ParseIntPipe) limit: number) {
     try {
       const comments = await this.commentService.GetRecentComments(limit);
@@ -102,12 +115,19 @@ export class CommentController {
   // Endpoint to create a comment with userID and postID
   @Post(':userId')
   @UsePipes(new DtoValidationPipe())
+  @ApiResponse({ status: 201, description: 'Create a comment' })
+  @ApiBadRequestResponse({ description: 'Invalid input data' })
+  @UsePipes(new DtoValidationPipe())
   async createPost(
     @Param('userId', ParseIntPipe) userId: number,
     @Param('postId', ParseIntPipe) postId: number,
     @Body() createCommentDto: CreateCommentDto,
   ) {
-    const comment = await this.commentService.CreateComment(userId, postId, createCommentDto);
+    const comment = await this.commentService.CreateComment(
+      userId,
+      postId,
+      createCommentDto,
+    );
     return { message: 'Comentário criado com sucesso!', comment };
   }
 
@@ -126,7 +146,6 @@ export class CommentController {
     @Body() patchCommentDto: PatchCommentDto,
   ) {
     try {
-
       if (patchCommentDto.content) {
         const trimmedcontent = patchCommentDto.content.trim();
 
@@ -135,9 +154,15 @@ export class CommentController {
         }
       }
 
-      const updatedComment = await this.commentService.PatchComment(id, patchCommentDto);
+      const updatedComment = await this.commentService.PatchComment(
+        id,
+        patchCommentDto,
+      );
 
-      return { message: 'Comentário atualizado com sucesso!', comment: updatedComment };
+      return {
+        message: 'Comentário atualizado com sucesso!',
+        comment: updatedComment,
+      };
     } catch (error) {
       return { error: 'Erro ao atualizar comentário. ' + error.message };
     }
