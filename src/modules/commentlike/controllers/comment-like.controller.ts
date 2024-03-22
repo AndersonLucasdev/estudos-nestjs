c195a2c16acc11afd7e757f1d792c475116aaa52
anderson <andinlucas8@gmail.com>
2024-03-22 20:08:59 -0300

    feat/ comment-like.controllers.ts: Terminando de adicionar o swagger

--- src/modules/commentlike/controllers/comment-like.controller.ts
+++ src/modules/commentlike/controllers/comment-like.controller.ts
@@ -33,6 +33,9 @@ export class CommentLikeController {
 
   // EndPoint Returns the number of likes on a specific comment
   @Get(':commentId/likes')
+  @ApiParam({ name: 'commentId', description: 'Comment ID' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Number of likes on the comment' })
+  @ApiNotFoundResponse({ description: 'Comment not found' })
   async getLikesInComment(
     @Param('commentId') commentId: number,
   ): Promise<number> {
@@ -41,6 +44,9 @@ export class CommentLikeController {
 
   // EndPoint Adds a like to a specific comment based on the provided user ID
   @Post(':commentId/likes')
+  @ApiParam({ name: 'commentId', description: 'Comment ID' })
+  @ApiResponse({ status: HttpStatus.CREATED, description: 'Liked the comment successfully' })
+  @ApiNotFoundResponse({ description: 'Comment not found' })
   async likeComment(
     @Param('commentId') commentId: number,
     @Body('userId') userId: number,
@@ -50,6 +56,10 @@ export class CommentLikeController {
 
   // EndPoint Unlikes a specific comment based on the provided user and comment IDs
   @Delete(':commentId/likes/:userId')
+  @ApiParam({ name: 'commentId', description: 'Comment ID' })
+  @ApiParam({ name: 'userId', description: 'User ID' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Like removed successfully' })
+  @ApiNotFoundResponse({ description: 'Comment or user not found' })
   async removeLikeFromComment(
     @Param('commentId') commentId: number,
     @Param('userId') userId: number,
