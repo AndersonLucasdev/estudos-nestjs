49df2c6fffc37630cfa3d0c4577e527e0d7a6d3a
anderson <andinlucas8@gmail.com>
2024-03-29 10:48:49 -0300

    feat/ notification.module.ts: Terminando de criar o module

--- src/modules/notification/modules/notification.module.ts
+++ src/modules/notification/modules/notification.module.ts
@@ -1,4 +1,16 @@
-import {MiddlewareConsumer, Module, NestModule, RequestMethod} from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { NotificationController } from '../controllers/notification.controller'; 
-import { NotificationService } from '../services/notification.service'; 
+import { NotificationController } from '../controllers/notification.controller';
+import { NotificationService } from '../services/notification.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [NotificationController],
+  providers: [NotificationService],
+})
+export class NotificationModule {}
