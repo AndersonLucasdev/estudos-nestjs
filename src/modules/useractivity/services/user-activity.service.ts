c6905b6115c07dd91f1ff48f94c41cd341c33538
anderson <andinlucas8@gmail.com>
2024-04-03 23:54:38 -0300

    feat/ user-activity.service.ts: Começando a classe

--- src/modules/useractivity/services/user-activity.service.ts
+++ src/modules/useractivity/services/user-activity.service.ts
@@ -1,13 +1,20 @@
 import {
-    Injectable,
-    NotFoundException,
-    BadRequestException,
-    ConflictException,
-  } from '@nestjs/common';
-  import { PrismaService } from 'src/prisma/prisma.service';
-  import { UserActivity } from '@prisma/client';
-  import { CreateUserActivityDto } from '../dto/CreateUserActivity.dto'; 
-  import { PatchUserActivityDto } from '../dto/PatchUserActivity.dto'; 
-  import * as bcrypt from 'bcrypt';
-  import { TrimSpaces } from 'src/utils/helpers';
-  import { WebSocketService } from 'src/modules/websocket/websocket.service';
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  ConflictException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserActivity } from '@prisma/client';
+import { CreateUserActivityDto } from '../dto/CreateUserActivity.dto';
+import { PatchUserActivityDto } from '../dto/PatchUserActivity.dto';
+import * as bcrypt from 'bcrypt';
+import { TrimSpaces } from 'src/utils/helpers';
+import { WebSocketService } from 'src/modules/websocket/websocket.service';
+
+@Injectable()
+export class UserActivityService {
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
+}
