3618b845fedcdc3862d439ae2a305e9f20e0170d
anderson <andinlucas8@gmail.com>
2024-04-08 21:16:45 -0300

    feat/ user-activity.controller.ts: Adicionando esse métodos no controller

--- src/modules/useractivity/controllers/user-activity.controller.ts
+++ src/modules/useractivity/controllers/user-activity.controller.ts
@@ -23,24 +23,49 @@ import { CreateUserActivityDto } from '../dto/CreateUserActivity.dto';
 
 @Controller('posts')
 export class UserActivityController {
-    constructor(private readonly userActivityService: UserActivityService) {}
+  constructor(private readonly userActivityService: UserActivityService) {}
 
-    @Get('/:userId')
-  async getUserActivities(@Param('userId') userId: number): Promise<UserActivity[]> {
+  @Get('/:userId')
+  async getUserActivities(
+    @Param('userId') userId: number,
+  ): Promise<UserActivity[]> {
     return this.userActivityService.getUserActivities(userId);
   }
 
   @Get('/:activityId')
-  async getUserActivityById(@Param('activityId') activityId: number): Promise<UserActivity> {
-    const activity = await this.userActivityService.getUserActivityById(activityId);
+  async getUserActivityById(
+    @Param('activityId') activityId: number,
+  ): Promise<UserActivity> {
+    const activity = await this.userActivityService.getUserActivityById(
+      activityId,
+    );
     if (!activity) {
       throw new NotFoundException('User activity not found.');
     }
     return activity;
   }
 
+  @Get('/:userId/type/:activityType')
+  async getUserActivitiesByType(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Param('activityType') activityType: UserActivityType
+  ): Promise<UserActivity[]> {
+    return this.userActivityService.getUserActivitiesByType(userId, activityType);
+  }
+
+  @Get('/:userId/type/:activityType/count')
+  async countUserActivitiesByType(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Param('activityType') activityType: UserActivityType
+  ): Promise<number> {
+    return this.userActivityService.countUserActivitiesByType(userId, activityType);
+  }
+
+
   @Post()
-  async createUserActivity(@Body() createUserActivityDto: CreateUserActivityDto): Promise<UserActivity> {
+  async createUserActivity(
+    @Body() createUserActivityDto: CreateUserActivityDto,
+  ): Promise<UserActivity> {
     try {
       return this.userActivityService.createUserActivity(createUserActivityDto);
     } catch (error) {
@@ -53,9 +78,15 @@ export class UserActivityController {
   }
 
   @Patch('/:activityId')
-  async updateUserActivity(@Param('activityId') activityId: number, @Body() patchUserActivityDto: PatchUserActivityDto): Promise<UserActivity> {
+  async updateUserActivity(
+    @Param('activityId') activityId: number,
+    @Body() patchUserActivityDto: PatchUserActivityDto,
+  ): Promise<UserActivity> {
     try {
-      return this.userActivityService.updateUserActivity(activityId, patchUserActivityDto);
+      return this.userActivityService.updateUserActivity(
+        activityId,
+        patchUserActivityDto,
+      );
     } catch (error) {
       if (error instanceof NotFoundException) {
         throw new NotFoundException('User activity not found.');
@@ -68,7 +99,9 @@ export class UserActivityController {
   }
 
   @Delete('/:activityId')
-  async deleteUserActivity(@Param('activityId') activityId: number): Promise<void> {
+  async deleteUserActivity(
+    @Param('activityId') activityId: number,
+  ): Promise<void> {
     try {
       await this.userActivityService.deleteUserActivity(activityId);
     } catch (error) {
