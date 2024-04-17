f4c8d5728a97046af5b56158b9c78b1e42b7377c
anderson <andinlucas8@gmail.com>
2024-04-16 21:26:48 -0300

    feat/ story.service.ts: Começando service

---
+++ src/modules/story/services/story.service.ts
@@ -0,0 +1,12 @@
+import {
+    Injectable,
+    NotFoundException,
+    BadRequestException,
+    ConflictException,
+  } from '@nestjs/common';
+
+
+@Injectable()
+export class CommentService {
+
+}
