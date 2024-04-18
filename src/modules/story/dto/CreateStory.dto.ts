eaee5dc424720ec1163477394dea25da4d3ba5f6
anderson <andinlucas8@gmail.com>
2024-04-18 19:36:48 -0300

    feat/ CreateStory.dto.ts: Finalizado

--- src/modules/story/dto/CreateStory.dto.ts
+++ src/modules/story/dto/CreateStory.dto.ts
@@ -12,3 +12,27 @@ import {
 } from 'class-validator';
 import { IsNotEmptyString } from 'src/decorators/IsNot-Empty-String.decorator';
 import { NotificationType } from '@prisma/client';
+
+export class CreateStoryDto {
+
+  @IsInt()
+  userId: number;
+
+  @IsOptional()
+  @IsInt()
+  postId?: number;
+
+  @IsOptional()
+  @IsString()
+  image?: string;
+
+  @IsDate()
+  creationDate: Date;
+
+  @IsInt()
+  viewCount: number;
+
+  // @IsOptional()
+  // @IsInt({ each: true })
+  // replies?: number[];
+}
