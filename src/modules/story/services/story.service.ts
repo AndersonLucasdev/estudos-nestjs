a1a2ebd290b120e8ab8db66d0d328eb1d01ca8e8
anderson <andinlucas8@gmail.com>
2024-04-19 20:39:45 -0300

    feat/ story.service.ts: Criando o CRUD para story

--- src/modules/story/services/story.service.ts
+++ src/modules/story/services/story.service.ts
@@ -19,5 +19,34 @@ export class StoryService {
     private readonly webSocketService: WebSocketService,
   ) {}
 
-  
+  async GetStoryById(id: number): Promise<Story> {
+    const story = await this.prisma.story.findUnique({ where: { id } });
+    if (!story) {
+      throw new NotFoundException('História não encontrada.');
+    }
+    return story;
+  }
+
+  async CreateStory(storyData: CreateStoryDto): Promise<Story> {
+    const createdStory = await this.prisma.story.create({ data: storyData });
+    return createdStory;
+  }
+
+  async UpdateStory(id: number, storyData: PatchStoryDto): Promise<Story> {
+    const updatedStory = await this.prisma.story.update({
+      where: { id },
+      data: storyData,
+    });
+    if (!updatedStory) {
+      throw new NotFoundException(`História com ID ${id} não encontrada`);
+    }
+    return updatedStory;
+  }
+
+  async DeleteStory(id: number): Promise<void> {
+    const deletedStory = await this.prisma.story.delete({ where: { id } });
+    if (!deletedStory) {
+      throw new NotFoundException(`História com ID ${id} não encontrada`);
+    }
+  }
 }
