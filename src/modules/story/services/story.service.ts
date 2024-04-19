8d197798e67694c2ff3cca398c8504c7ddba89b6
anderson <andinlucas8@gmail.com>
2024-04-19 20:34:44 -0300

    feat/ story.service.ts: Fazendo as importações necessárias para service

--- src/modules/story/services/story.service.ts
+++ src/modules/story/services/story.service.ts
@@ -1,18 +1,16 @@
 import {
-    Injectable,
-    NotFoundException,
-    BadRequestException,
-    ConflictException,
-  } from '@nestjs/common';
-  import { PrismaService } from 'src/prisma/prisma.service';
-  // import {  } from '@prisma/client';
-  import { CreateStoryDto } from '../dto/CreateStory.dto';
-  import { PatchStoryDto } from '../dto/PatchStory.dto';
-  import * as bcrypt from 'bcrypt';
-  import { TrimSpaces } from 'src/utils/helpers';
-  import { WebSocketService } from 'src/modules/websocket/websocket.service';
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  ConflictException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Story } from '@prisma/client';
+import { CreateStoryDto } from '../dto/CreateStory.dto';
+import { PatchStoryDto } from '../dto/PatchStory.dto';
+import * as bcrypt from 'bcrypt';
+import { TrimSpaces } from 'src/utils/helpers';
+import { WebSocketService } from 'src/modules/websocket/websocket.service';
 
 @Injectable()
-export class StoryService {
-
-}
+export class StoryService {}
