adbffd1edba78ac972bd4cd5c0f97c8237bfc08e
anderson <andinlucas8@gmail.com>
2024-04-20 17:35:08 -0300

    feat/ story.controller.ts: Implementando o CRUD principal

--- src/modules/story/controllers/story.controller.ts
+++ src/modules/story/controllers/story.controller.ts
@@ -22,9 +22,38 @@ import { CreateUserDto } from 'src/modules/user/dto/CreatUser.dto';
 import { PatchUserDto } from 'src/modules/user/dto/PatchUser.dto';
 import { WebSocketService } from 'src/modules/websocket/websocket.service';
 
-
+@Controller('stories')
 export class StoryController {
   constructor(private readonly storyService: StoryService) {}
 
-  
+  @Get(':id')
+  async getStoryById(@Param('id') id: number): Promise<Story> {
+    const story = await this.storyService.getStoryById(id);
+    if (!story) {
+      throw new NotFoundException('História não encontrada.');
+    }
+    return story;
+  }
+
+  @Post()
+  async createStory(@Body() storyData: CreateStoryDto): Promise<Story> {
+    return this.storyService.createStory(storyData);
+  }
+
+  @Patch(':id')
+  async updateStory(@Param('id') id: number, @Body() storyData: PatchStoryDto): Promise<Story> {
+    const updatedStory = await this.storyService.updateStory(id, storyData);
+    if (!updatedStory) {
+      throw new NotFoundException(`História com ID ${id} não encontrada`);
+    }
+    return updatedStory;
+  }
+
+  @Delete(':id')
+  async deleteStory(@Param('id') id: number): Promise<void> {
+    const deletedStory = await this.storyService.deleteStory(id);
+    if (!deletedStory) {
+      throw new NotFoundException(`História com ID ${id} não encontrada`);
+    }
+  }
 }
