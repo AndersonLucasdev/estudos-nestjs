6d9d1327cfca9db285374c7067b3fa04c30e01c5
anderson <andinlucas8@gmail.com>
2024-04-20 17:28:16 -0300

    feat/ story.controller.ts: Adicionando as importações necessárias

--- src/modules/story/controllers/story.controller.ts
+++ src/modules/story/controllers/story.controller.ts
@@ -1,21 +1,26 @@
 import {
-    Controller,
-    Get,
-    Post,
-    Patch,
-    Delete,
-    Body,
-    Param,
-    Query,
-    ParseIntPipe,
-    UsePipes,
-    NotFoundException,
-    HttpStatus,
-    ConflictException,
-    BadRequestException,
-    HttpException,
-  } from '@nestjs/common';
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+  ParseIntPipe,
+  UsePipes,
+  NotFoundException,
+  HttpStatus,
+  ConflictException,
+  BadRequestException,
+  HttpException,
+} from '@nestjs/common';
+import { StoryService } from '../services/story.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from '@prisma/client';
+import { CreateUserDto } from 'src/modules/user/dto/CreatUser.dto';
+import { PatchUserDto } from 'src/modules/user/dto/PatchUser.dto';
+import { WebSocketService } from 'src/modules/websocket/websocket.service';
 
-export class StoryController {
 
-}
+export class StoryController {}
