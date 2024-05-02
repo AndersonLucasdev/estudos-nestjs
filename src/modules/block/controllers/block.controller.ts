2d33ea72e3be114eb3e83e288cdba3c420c04f20
anderson <andinlucas8@gmail.com>
2024-05-02 20:44:52 -0300

    feat/ block.controller.ts: Criando a classe BlockController

---
+++ src/modules/block/controllers/block.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+  ParseIntPipe,
+  UsePipes,
+  NotFoundException,
+  HttpStatus,
+  ConflictException,
+  BadRequestException,
+  HttpException,
+} from '@nestjs/common';
+import { BlockService } from '../services/block.service';
+import { CreateBlockDto } from '../dto/CreateBlock.dto';
+import { PatchBlockDto } from '../dto/PatchBlock.dto';
+import { DtoValidationPipe } from 'src/pipes/dto-validation.pipe';
+
+@Controller('comments')
+export class BlockController {}
