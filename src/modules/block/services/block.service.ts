c3f757e3b11756910cae04147f4b2537c96da579
anderson <andinlucas8@gmail.com>
2024-05-02 20:43:07 -0300

    feat/ block.service.ts: Criando a classe BlockService

---
+++ src/modules/block/services/block.service.ts
@@ -0,0 +1,14 @@
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  ConflictException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Block } from '@prisma/client';
+import { CreateCommentDto } from 'src/modules/comment/dto/CreateComment.dto';
+import { PatchCommentDto } from 'src/modules/comment/dto/PatchComment.dto';
+import { WebSocketService } from 'src/modules/websocket/websocket.service';
+
+@Injectable()
+export class BlockService {}
