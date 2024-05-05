bf2c49951cdf7afe562e8d46273c7b36e7137e0c
anderson <andinlucas8@gmail.com>
2024-05-05 18:44:55 -0300

    feat/ block.service.ts: Começando a criar os novos métodos

--- src/modules/block/services/block.service.ts
+++ src/modules/block/services/block.service.ts
@@ -14,5 +14,24 @@ import { WebSocketService } from 'src/modules/websocket/websocket.service';
 export class BlockService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async createBlock(userId: number, blockedUserId: number): Promise<Block> {
+    return this.prisma.block.create({
+      data: {
+        userId,
+        blockedUserId,
+      },
+    });
+  }
 
+  async unlockUser(userId: number, blockedUserId: number): Promise<void> {
+    const block = await this.prisma.block.findUnique({
+      where: { userId_blockedUserId: { userId, blockedUserId } },
+    });
+    if (!block) {
+      throw new NotFoundException('Block not found.');
+    }
+    await this.prisma.block.delete({
+      where: { userId_blockedUserId: { userId, blockedUserId } },
+    });
+  }
 }
