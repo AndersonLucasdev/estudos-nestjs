36212663cfee8357ef72dc90f895dbf2ab3fb01c
anderson <andinlucas8@gmail.com>
2024-05-05 18:59:15 -0300

    feat/ block.controller.ts: Começando os métodos do controller

--- src/modules/block/controllers/block.controller.ts
+++ src/modules/block/controllers/block.controller.ts
@@ -24,5 +24,33 @@ import { DtoValidationPipe } from 'src/pipes/dto-validation.pipe';
 export class BlockController {
   constructor(private readonly blockService: BlockService) {}
 
-  
+  @Post(':userId/:blockedUserId')
+  async createBlock(
+    @Param('userId') userId: number,
+    @Param('blockedUserId') blockedUserId: number,
+  ): Promise<void> {
+    try {
+      await this.blockService.createBlock(userId, blockedUserId);
+    } catch (error) {
+      if (error instanceof ConflictException) {
+        throw new ConflictException('User already blocked.');
+      }
+      throw error;
+    }
+  }
+
+  @Delete(':userId/:blockedUserId')
+  async unlockUser(
+    @Param('userId') userId: number,
+    @Param('blockedUserId') blockedUserId: number,
+  ): Promise<void> {
+    try {
+      await this.blockService.unlockUser(userId, blockedUserId);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException('Block not found.');
+      }
+      throw error;
+    }
+  }
 }
