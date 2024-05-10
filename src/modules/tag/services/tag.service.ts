6c57a81abf49e350ed45c9e44fdeac5f7beed061
anderson <andinlucas8@gmail.com>
2024-05-10 20:59:27 -0300

    feat/ tag.service.ts: Criando novo service TagService

---
+++ src/modules/tag/services/tag.service.ts
@@ -0,0 +1,21 @@
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  ConflictException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Tag } from '@prisma/client';
+// import { CreateTagDto }
+// import { PatchSTagDto }
+import * as bcrypt from 'bcrypt';
+import { TrimSpaces } from 'src/utils/helpers';
+import { WebSocketService } from 'src/modules/websocket/websocket.service';
+
+@Injectable()
+export class TagService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly webSocketService: WebSocketService,
+  ) {}
+}
