2e994e47c06a70629fd24900fb21d79afae03c1e
anderson <andinlucas8@gmail.com>
2024-05-15 21:20:31 -0300

    feat/ tag.controller.ts: CRUD concluido

--- src/modules/tag/controllers/tag.controller.ts
+++ src/modules/tag/controllers/tag.controller.ts
@@ -25,13 +25,27 @@ import { WebSocketService } from 'src/modules/websocket/websocket.service';
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
+  @Get(':id')
+  async getTagById(@Param('id', ParseIntPipe) id: number): Promise<Tag> {
+    return this.tagService.getTagById(id);
+  }
+
   @Post()
   async createTag(@Body() createTagDto: CreateTagDto): Promise<Tag> {
     return this.tagService.createTag(createTagDto);
   }
 
-  @Get(':id')
-  async getTagById(@Param('id', ParseIntPipe) id: number): Promise<Tag> {
-    return this.tagService.getTagById(id);
+  @Delete(':id')
+  async deleteTag(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tagService.deleteTag(id);
   }
+
+  @Patch(':id')
+  async updateTag(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() patchTagDto: PatchTagDto,
+  ): Promise<Tag> {
+    return this.tagService.updateTag(id, patchTagDto);
+  }
+
 }
