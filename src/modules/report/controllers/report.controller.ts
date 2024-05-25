8914373a6b47a0d1d0a6133e846e63510c79e82c
anderson <andinlucas8@gmail.com>
2024-05-25 20:28:30 -0300

    feat/ report.controller.ts: Começando a criação dos métodos ReportController

--- src/modules/report/controllers/report.controller.ts
+++ src/modules/report/controllers/report.controller.ts
@@ -25,4 +25,17 @@ import { WebSocketService } from 'src/modules/websocket/websocket.service';
 @Controller('reports')
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
+
+  @Get(':id')
+  async getReportById(@Param('id', ParseIntPipe) id: number): Promise<Report> {
+    try {
+      const report = await this.reportService.getReportById(id);
+      if (!report) {
+        throw new NotFoundException('Tag not found.');
+      }
+      return report
+    } catch (error) {
+      throw new NotFoundException('Report not found.');
+    }
+  }
 }
