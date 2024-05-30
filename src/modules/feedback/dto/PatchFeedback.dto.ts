f5493da9836cf4ac3a5d0f089375ad722ed4af49
anderson <andinlucas8@gmail.com>
2024-05-30 19:05:43 -0300

    feat/ PatchFeedback.dto.ts: Concluindo Patchdto

--- src/modules/feedback/dto/PatchFeedback.dto.ts
+++ src/modules/feedback/dto/PatchFeedback.dto.ts
@@ -2,6 +2,20 @@ import { IsString, IsInt, IsOptional, Min, Max } from 'class-validator';
 import { IsNotEmptyString } from 'src/decorators/IsNot-Empty-String.decorator';
 
 export class PatchFeedbackDto {
+  @IsInt()
+  @IsOptional()
+  userId?: number;
 
-    
+  @IsNotEmptyString()
+  @IsOptional()
+  content?: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  @IsOptional()
+  rating?: number;
+
+  @IsOptional()
+  createdAt?: Date;
 }
