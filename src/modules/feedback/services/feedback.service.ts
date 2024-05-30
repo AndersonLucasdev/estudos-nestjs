3f0f3872dd5dc0495591b57c49c6a48d59fcff36
anderson <andinlucas8@gmail.com>
2024-05-30 19:09:42 -0300

    feat/ feedback.service.ts: Começando a criação do FeedbackService

---
+++ src/modules/feedback/services/feedback.service.ts
@@ -0,0 +1,15 @@
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  ConflictException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateFeedbackDto } from '../dto/CreateFeedback.dto';
+import { PatchFeedbackDto } from '../dto/PatchFeedback.dto';
+import { Feedback } from '@prisma/client';
+
+@Injectable()
+export class FeedbackService {
+  constructor(private readonly prisma: PrismaService) {}
+}
