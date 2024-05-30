4f2ef4f19c46fa570dde9b58bc3d72937199a93c
anderson <andinlucas8@gmail.com>
2024-05-30 19:03:28 -0300

    feat/ CreateFeedback.dto.ts: Começando a criação do Createdto

---
+++ src/modules/feedback/dto/CreateFeedback.dto.ts
@@ -0,0 +1,13 @@
+import {
+    IsString,
+    IsInt,
+    IsDate,
+    IsOptional,
+    Min,
+    Max,
+  } from 'class-validator';
+  import { IsNotEmptyString } from 'src/decorators/IsNot-Empty-String.decorator';
+  
+  export class CreateFeedbackDto {
+    
+  }
