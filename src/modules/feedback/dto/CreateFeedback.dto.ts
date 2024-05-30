cc65ad31bfd5b9c6c7fbe14a86305a8bad013580
anderson <andinlucas8@gmail.com>
2024-05-30 19:05:14 -0300

    feat/ PatchFeedback.dto.ts: Começando a construção do PatchFeedbackDto

--- src/modules/feedback/dto/CreateFeedback.dto.ts
+++ src/modules/feedback/dto/CreateFeedback.dto.ts
@@ -1,26 +1,19 @@
-import {
-    IsString,
-    IsInt,
-    IsDate,
-    IsOptional,
-    Min,
-    Max,
-  } from 'class-validator';
-  import { IsNotEmptyString } from 'src/decorators/IsNot-Empty-String.decorator';
-  
-  export class CreateFeedbackDto {
-    @IsInt()
-    userId: number;
-  
-    @IsNotEmptyString()
-    content: string;
-  
-    @IsInt()
-    @Min(1)
-    @Max(5)
-    rating: number;
-  
-    @IsDate()
-    @IsOptional()
-    createdAt?: Date;
-  }
+import { IsString, IsInt, IsDate, IsOptional, Min, Max } from 'class-validator';
+import { IsNotEmptyString } from 'src/decorators/IsNot-Empty-String.decorator';
+
+export class CreateFeedbackDto {
+  @IsInt()
+  userId: number;
+
+  @IsNotEmptyString()
+  content: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  rating: number;
+
+  @IsDate()
+  @IsOptional()
+  createdAt?: Date;
+}
---
+++ src/modules/feedback/dto/PatchFeedback.dto.ts
@@ -0,0 +1,7 @@
+import { IsString, IsInt, IsOptional, Min, Max } from 'class-validator';
+import { IsNotEmptyString } from 'src/decorators/IsNot-Empty-String.decorator';
+
+export class PatchFeedbackDto {
+
+    
+}
