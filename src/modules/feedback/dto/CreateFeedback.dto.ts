2515cd8a80fa204fe4acaf709786a84794257f4b
anderson <andinlucas8@gmail.com>
2024-05-30 19:03:51 -0300

    feat/ CreateFeedback.dto.ts: Concluindo CreateDto

--- src/modules/feedback/dto/CreateFeedback.dto.ts
+++ src/modules/feedback/dto/CreateFeedback.dto.ts
@@ -9,5 +9,18 @@ import {
   import { IsNotEmptyString } from 'src/decorators/IsNot-Empty-String.decorator';
   
   export class CreateFeedbackDto {
-    
+    @IsInt()
+    userId: number;
+  
+    @IsNotEmptyString()
+    content: string;
+  
+    @IsInt()
+    @Min(1)
+    @Max(5)
+    rating: number;
+  
+    @IsDate()
+    @IsOptional()
+    createdAt?: Date;
   }
