5e39defcabf02e1a4e61ba101621ca9ae052dcaa
anderson <andinlucas8@gmail.com>
2024-05-31 20:06:44 -0300

    feat/ feedback.controller.ts: Criando o FeedbackController

---
+++ src/modules/feedback/controllers/feedback.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+  NotFoundException,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiBody } from '@nestjs/swagger';
+import { FeedbackService } from '../services/feedback.service';
+import { CreateFeedbackDto } from '../dto/CreateFeedback.dto';
+import { PatchFeedbackDto } from '../dto/PatchFeedback.dto';
+import { Feedback } from '@prisma/client';
+
+@Controller('feedbacks')
+export class FeedbackController {
+
+    
+}
