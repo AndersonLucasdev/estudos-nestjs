03090a754e1725b29cc3eee7144a009713253168
anderson <andinlucas8@gmail.com>
2024-06-29 23:19:06 -0300

    feat/ user.controller.spec.ts: Criando testes UserController

--- src/modules/user/tests/user.controller.spec.ts
+++ src/modules/user/tests/user.controller.spec.ts
@@ -8,3 +8,27 @@ import {
   ConflictException,
   NotFoundException,
 } from '@nestjs/common';
+
+describe('UserController', () => {
+  let controller: UserController;
+  let service: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            GetAllUsers: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
+  });
+
+  
+});
