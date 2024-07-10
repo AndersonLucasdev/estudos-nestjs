f4fc0654274daf43ad1dbf7cafcf5f90ef22c054
anderson <andinlucas8@gmail.com>
2024-07-09 21:18:52 -0300

    feat/ user.service.spec.ts: Começando os testes para UserService

--- src/modules/user/tests/user.service.spec.ts
+++ src/modules/user/tests/user.service.spec.ts
@@ -10,3 +10,47 @@ import {
   ConflictException,
   BadRequestException,
 } from '@nestjs/common';
+
+describe('UserService', () => {
+  let service: UserService;
+  let prisma: PrismaService;
+  let webSocketService: WebSocketService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+              findFirst: jest.fn(),
+              create: jest.fn(),
+              delete: jest.fn(),
+              update: jest.fn(),
+            },
+            block: {
+              findMany: jest.fn(),
+            },
+            userFollowers: {
+              findMany: jest.fn(),
+            },
+          },
+        },
+        {
+          provide: WebSocketService,
+          useValue: {
+            addUserConnection: jest.fn(),
+            removeUserConnection: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+    prisma = module.get<PrismaService>(PrismaService);
+    webSocketService = module.get<WebSocketService>(WebSocketService);
+  });
+});
