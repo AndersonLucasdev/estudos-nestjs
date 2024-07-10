123be24c56596ca03117f6e0c2c83cb938eafb2d
anderson <andinlucas8@gmail.com>
2024-07-09 21:44:36 -0300

    feat/ user.service.spec.ts: Concluindo método de teste para CreateUser

--- src/modules/user/tests/user.service.spec.ts
+++ src/modules/user/tests/user.service.spec.ts
@@ -5,6 +5,7 @@ import { CreateUserDto } from '../dto/CreatUser.dto';
 import { PatchUserDto } from '../dto/PatchUser.dto';
 import { UserActivityType } from '@prisma/client';
 import { NotificationType } from '@prisma/client';
+
 import { WebSocketService } from 'src/modules/websocket/websocket.service';
 import {
   NotFoundException,
@@ -53,5 +54,81 @@ describe('UserService', () => {
     service = module.get<UserService>(UserService);
     prisma = module.get<PrismaService>(PrismaService);
     webSocketService = module.get<WebSocketService>(WebSocketService);
+  });
+
+  describe('CreateUser', () => {
+    it('should create a new user', async () => {
+      const createUserDto: CreateUserDto = {
+        username: 'user1',
+        email: 'user1@example.com',
+        password: 'password',
+        confirmPassword: 'password',
+        name: 'User One'
+      };
+      const mockUser = { id: 1, ...createUserDto };
+
+      prisma.user.findFirst = jest.fn().mockResolvedValue(null);
+      prisma.user.create = jest.fn().mockResolvedValue(mockUser);
+      const bcryptHashSpy = jest.spyOn(bcrypt, 'hash').mockResolvedValue('hashedPassword');
+      const addUserConnectionSpy = jest.spyOn(webSocketService, 'addUserConnection').mockImplementation();
+
+      const result = await service.CreateUser(createUserDto);
+
+      expect(result).toEqual(mockUser);
+      expect(prisma.user.findFirst).toHaveBeenCalledWith({ where: { email: createUserDto.email.trim() } });
+      expect(prisma.user.findFirst).toHaveBeenCalledWith({ where: { username: createUserDto.username.trim() } });
+      expect(bcryptHashSpy).toHaveBeenCalledWith(createUserDto.password.trim(), 10);
+      expect(prisma.user.create).toHaveBeenCalledWith({
+        data: {
+          ...createUserDto,
+          email: createUserDto.email.trim(),
+          username: createUserDto.username.trim(),
+          password: 'hashedPassword',
+        },
+      });
+      expect(addUserConnectionSpy).toHaveBeenCalledWith(mockUser.id, mockUser.connectionId);
+    });
+
+    it('should throw ConflictException if email is already in use', async () => {
+      const createUserDto: CreateUserDto = {
+        username: 'user1',
+        email: 'user1@example.com',
+        password: 'password',
+        confirmPassword: 'password',
+        name: 'User One'
+      };
+      const existingUser = { id: 1, ...createUserDto };
+
+      prisma.user.findFirst = jest.fn().mockResolvedValue(existingUser);
+
+      await expect(service.CreateUser(createUserDto)).rejects.toThrow(ConflictException);
+    });
+
+    it('should throw ConflictException if username is already in use', async () => {
+      const createUserDto: CreateUserDto = {
+        username: 'user1',
+        email: 'user1@example.com',
+        password: 'password',
+        confirmPassword: 'password',
+        name: 'User One'
+      };
+      const existingUser = { id: 1, ...createUserDto };
+
+      prisma.user.findFirst = jest.fn().mockResolvedValueOnce(null).mockResolvedValueOnce(existingUser);
+
+      await expect(service.CreateUser(createUserDto)).rejects.toThrow(ConflictException);
+    });
+
+    it('should throw BadRequestException if passwords do not match', async () => {
+      const createUserDto: CreateUserDto = {
+        username: 'user1',
+        email: 'user1@example.com',
+        password: 'password',
+        confirmPassword: 'differentPassword',
+        name: 'User One'
+      };
+
+      await expect(service.CreateUser(createUserDto)).rejects.toThrow(BadRequestException);
+    });
   });
 });
