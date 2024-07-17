2087e68c855d0a90705f70caaafb1b2212c95c2a
anderson <andinlucas8@gmail.com>
2024-07-17 20:17:49 -0300

    feat/ story.controller.spec.ts: Testes para os métodos CRUD

--- src/modules/story/tests/story.controller.spec.ts
+++ src/modules/story/tests/story.controller.spec.ts
@@ -60,4 +60,110 @@ describe('StoryController', () => {
       );
     });
   });
+
+  describe('createStory', () => {
+    it('should create a new story', async () => {
+      const createStoryDto: CreateStoryDto = {
+        userId: 1,
+        postId: null,
+        image: null,
+        creationDate: new Date(),
+        viewCount: 1,
+      };
+      const mockStory: Story = {
+        id: 1,
+        creationDate: new Date(),
+        viewCount: 1,
+        userId: 1,
+        postId: null,
+        image: null,
+        expirationDate: null,
+      };
+      jest.spyOn(service, 'CreateStory').mockResolvedValue(mockStory);
+
+      const result = await controller.createStory(createStoryDto);
+      expect(result).toEqual(mockStory);
+      expect(service.CreateStory).toHaveBeenCalledWith(createStoryDto);
+    });
+
+    it('should throw ConflictException if error creating story', async () => {
+      const createStoryDto: CreateStoryDto = {
+        userId: 1,
+        postId: null,
+        image: null,
+        creationDate: new Date(),
+        viewCount: 1,
+      };
+      jest
+        .spyOn(service, 'CreateStory')
+        .mockRejectedValue(new ConflictException());
+
+      await expect(controller.createStory(createStoryDto)).rejects.toThrow(
+        ConflictException,
+      );
+    });
+  });
+
+  describe('updateStory', () => {
+    it('should update a story by ID', async () => {
+      const patchStoryDto: PatchStoryDto = { viewCount: 10 };
+      const mockStory: Story = {
+        id: 1,
+        creationDate: new Date(),
+        viewCount: 1,
+        userId: 1,
+        postId: null,
+        image: null,
+        expirationDate: null,
+      };
+      jest.spyOn(service, 'UpdateStory').mockResolvedValue(mockStory);
+
+      const result = await controller.updateStory(1, patchStoryDto);
+      expect(result).toEqual(mockStory);
+      expect(service.UpdateStory).toHaveBeenCalledWith(1, patchStoryDto);
+    });
+
+    it('should throw NotFoundException if story not found', async () => {
+      const patchStoryDto: PatchStoryDto = { viewCount: 10 };
+      jest
+        .spyOn(service, 'UpdateStory')
+        .mockRejectedValue(new NotFoundException());
+
+      await expect(controller.updateStory(1, patchStoryDto)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('deleteStory', () => {
+    it('should delete a story by ID', async () => {
+      const mockStory: Story = {
+        id: 1,
+        creationDate: new Date(),
+        viewCount: 1,
+        userId: 1,
+        postId: null,
+        image: null,
+        expirationDate: null,
+      };
+      jest.spyOn(service, 'DeleteStory').mockResolvedValue();
+      jest
+        .spyOn(service, 'GetStoryById')
+        .mockRejectedValue(new NotFoundException());
+
+      await controller.deleteStory(1);
+      expect(service.DeleteStory).toHaveBeenCalledWith(1);
+      expect(service.GetStoryById).toHaveBeenCalledWith(1);
+    });
+
+    it('should throw NotFoundException if story not found after deletion', async () => {
+      jest
+        .spyOn(service, 'DeleteStory')
+        .mockRejectedValue(new NotFoundException());
+
+      await expect(controller.deleteStory(1)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
 });
