67a546c9ff83106c11ecf16fb970397087ccc5bd
anderson <andinlucas8@gmail.com>
2024-07-17 20:14:02 -0300

    feat/ story.controller.spec.ts: Começando os testes StoryController

--- src/modules/story/tests/story.controller.spec.ts
+++ src/modules/story/tests/story.controller.spec.ts
@@ -6,9 +6,29 @@ import { NotificationType, UserActivityType } from '@prisma/client';
 import { Gender } from '@prisma/client';
 import { Story } from '@prisma/client';
 import { CreateStoryDto } from '../dto/CreateStory.dto';
+import { WebSocketService } from 'src/modules/websocket/websocket.service';
 import { PatchStoryDto } from '../dto/PatchStory.dto';
 import {
   BadRequestException,
   ConflictException,
   NotFoundException,
 } from '@nestjs/common';
+
+describe('StoryController', () => {
+  let controller: StoryController;
+  let service: StoryService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [StoryController],
+      providers: [StoryService, PrismaService, WebSocketService],
+    }).compile();
+
+    controller = module.get<StoryController>(StoryController);
+    service = module.get<StoryService>(StoryService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
